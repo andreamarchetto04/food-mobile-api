bf5868641261188d2d6846627b24dd3b68e1d712
Mattia-Zanini <mattzanini2@gmail.com>
2022-11-19 10:29:30 +0100

    .

--- api/Products/getArchiveIngredients.php
+++ api/Products/getArchiveIngredients.php
@@ -2,10 +2,11 @@
 require("../../db/connectDB.php");
 require("../../model/ProductController.php");
 
-$panino = $_GET["panino"];
+if (isset($_GET['panino']))
+    $panino = $_GET['panino'];
 $controller = new ProductController($conn);
 
-if ($panino) {
+if (!empty($_GET['panino'])) {
     $controller->GetProductIngredients($panino);
 } else {
     $controller->sendError(array('Content-Type: application/json', 'HTTP/1.1 200 OK'));
