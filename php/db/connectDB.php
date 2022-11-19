f78e01efbd100a9fe00c23c958e2706a4124a37a
Mattia-Zanini <mattzanini2@gmail.com>
2022-11-19 09:20:26 +0100

    cambiato struttura api

--- php/db/connectDB.php
+++ php/db/connectDB.php
@@ -1,15 +1,19 @@
 <?php
     // Properties
-    define("DB_HOST", "localhost");
-    define("DB_USERNAME", "root");
-    define("DB_DATABASE_NAME", "sandwiches");
-    define("DB_PORT", "3306");
-    define("DB_PASSWORD", "");
+    define("DB_HOST", "localhost"); //da cambiare con l'host del server
+    define("DB_USERNAME", "root"); //da cambiare con l'username del server
+    define("DB_DATABASE_NAME", "sandwiches"); //eventualmente da cambiare con nuovo nome del db
+    define("DB_PORT", "3306"); //non dovrebbe cambiare
+    define("DB_PASSWORD", ""); //da settare se necessaria
 
     //Connection
-    $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME, DB_PORT);
-    //echo print_r($conn);
+    $connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME, DB_PORT);
+    echo var_dump($connection);
 
-    if ($conn->connect_error)
-        die("Connection failed: " . $conn->connect_error);
+    if($connection === false)
+    {
+        die("<script>console.log('Errore di connessione!')<script>");
+    }
+
+    echo "<script>console.log('Connessione avvenuta con successo!')</script>";
 ?>
--- php/index.php
+++ php/index.php
@@ -1,16 +1,3 @@
 <?php
-require("db/connectDB.php");
-require("model/BaseController.php");
-
-$sql = "
-select distinct p.ID, p.name, p.price
-from product p
-order by p.ID;
-";
-
-$result = $conn->query($sql);
-$conn->close();
-
-$controller = new BaseController();
-$controller->sendOutput($result, array('Content-Type: application/json', 'HTTP/1.1 200 OK'));
+    echo 'silence is golden';
 ?>
